38efb101e8d0380443f8f4a183531631e1c75f17
Ahmad4789 <49160694+Ahmad4789@users.noreply.github.com>
2019-04-05 11:48:04 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("562584649163997190")
 setInterval(function() {
-channel.send(`سبام كريديت احم مدري تجربة اولى xd allo alslam 3likm hello`);
+channel.send(`انا خلصت من تجبرتي الاولى فيب يعني رح ابلش تجربتي الثانية وعلى طريق ال 200 الف بس الصراحة بجد هو صعب`);
 }, 30)
 })
 

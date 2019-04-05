1f14595bf7d04db7d6cee232cb874ade3499dc84
Ahmad4789 <49160694+Ahmad4789@users.noreply.github.com>
2019-04-05 02:26:44 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("562584649163997190")
+setInterval(function() {
+channel.send(`سبام كريديت احم مدري تجربة اولى xd allo alslam 3likm hello`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
